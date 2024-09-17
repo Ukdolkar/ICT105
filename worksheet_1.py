af3862ff5b860042fe266f7f7fd977e0edcf7c6c
Ukdolkar <12034514@sche.edu.au>
2024-09-17 20:09:57 +0800

    Add files via upload

---
+++ worksheet_1.py
@@ -0,0 +1,28 @@
+a = 15
+b = 12
+print(type(a))
+print(type(b))
+
+print(f"a +b ={a+b}")
+print(f"a-b ={a-b}")
+print(f"a*b = {a*b}")
+print(f"a/b = {a/b}")
+
+c = int(a/b)
+print(c)
+print(type(c))
+
+b = float(c)
+print(b)
+print(type(b))
+
+print(f"The result of a divided by b is{c}")
+
+if a>b :
+    print("True")
+else:
+    print("False")
+if a==b:
+    print("True")
+else:
+    print("False")
